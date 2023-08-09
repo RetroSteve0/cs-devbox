39e0db0c6c9f5c80fb1805d36c25fb972e63c175
Steven Mosley <34114851+RetroSteve0@users.noreply.github.com>
2023-08-09 20:14:55 +0000

    Added motd banner

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-# User setup
+# Prompt for username
 read -p "Enter username: " username
+
+# Prompt for password (without echoing to terminal)
 read -s -p "Enter password: " password
 echo
+
+# Generate a random salt of 16 bytes
 salt=$(head -c 16 /dev/urandom | base64)
+
+# Use the salt in password hashing
 hashed_password=$(openssl passwd -6 -salt $salt $password)
 
-# Create username with salted password
-sudo useradd -m -G sudo -p "$hashed_password" $username
+# Create new sudoer with hashed password and /bin/bash shell
+sudo useradd -m -s /bin/bash -G sudo -p "$hashed_password" $username
 
 # Update system
 sudo apt-get update
