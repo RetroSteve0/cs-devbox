d9b1e47c7637f1ef22bb3607115bf1f4987251fc
Steven Mosley <34114851+RetroSteve0@users.noreply.github.com>
2023-08-10 18:24:25 +0000

    Modified setup.sh to delay between steps so user can see what the script is currently doing

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -8,11 +8,27 @@ prompt_for_password() {
     echo
 }
 
+# Function to display animated progress
+animate_progress() {
+    local delay_seconds="$1"
+    local max_dots=3
+    local dots=""
+
+    for ((i = 0; i < max_dots; i++)); do
+        dots+="."
+        echo -ne "$dots\r"
+        sleep "$delay_seconds"
+    done
+    echo
+}
+
 # Step 1: Prompt for username
 read -p "Enter username: " username
 
+# Pause with animated progress
+animate_progress 1
+
 # Step 2: Prompt for passwords until they match
-echo "Enter and confirm password"
 while true; do
     prompt_for_password
 
@@ -23,18 +39,30 @@ while true; do
     fi
 done
 
+# Pause with animated progress
+animate_progress 1
+
 # Step 3: Generating a random salt
 echo "Generating a random salt"
 salt=$(openssl rand -hex 16)
+
+# Pause with animated progress
+animate_progress 1
 
 # Step 4: Hashing the password using salt
 echo "Hashing the password"
 hashed_password=$(openssl passwd -6 -salt "$salt" "$password")
 
+# Pause with animated progress
+animate_progress 1
+
 # Step 5: Creating the user account
 echo "Creating user account"
 sudo useradd -m -s /bin/bash -G sudo -p "$hashed_password" "$username"
 echo "User account created successfully."
+
+# Pause with animated progress
+animate_progress 1
 
 # Step 6: Updating the system
 echo "Updating the system"
