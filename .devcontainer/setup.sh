561137d075389218ffd3dad15e0adb851952341e
Steven Mosley <34114851+RetroSteve0@users.noreply.github.com>
2023-08-10 16:57:35 +0000

    I'm lazy

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-# Define a log file path
-LOG_FILE="/var/log/install_script.log"
-
-# Function to execute commands and log errors
-execute_command() {
-    "$@" 2>> "$LOG_FILE"
-}
-
-# Clear previous log content
-execute_command echo "Clearing log file from previous content..."
-> "$LOG_FILE"
-
 # Update system
-execute_command echo "Updating system..."
-execute_command sudo apt-get update -y
-execute_command sudo apt-get upgrade -y
-
-# Display the log file
-echo "Log file: $LOG_FILE"
+sudo apt-get update -y
+sudo apt-get upgrade -y
