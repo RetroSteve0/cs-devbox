c287cdfc48692bf81d14046bc7ef02c8a8bf442a
Steven Mosley <34114851+RetroSteve0@users.noreply.github.com>
2023-08-10 08:55:28 -0400

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,4 +4,16 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 
-# Add other commands here
+# Install NVM
+sudo git clone https://github.com/nvm-sh/nvm.git /usr/local/nvm
+sudo chown -R root:root /usr/local/nvm
+echo 'export NVM_DIR="$HOME/.nvm"' | sudo tee -a /etc/bash.bashrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' | sudo tee -a /etc/bash.bashrc
+echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"' | sudo tee -a /etc/bash.bashrc
+
+# Install Pyenv
+sudo git clone https://github.com/pyenv/pyenv.git /usr/local/pyenv
+sudo chown -R root:root /usr/local/pyenv
+echo 'export PYENV_ROOT="/usr/local/pyenv"' | sudo tee -a /etc/bash.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' | sudo tee -a /etc/bash.bashrc
+echo 'eval "$(pyenv init -)"' | sudo tee -a /etc/bash.bashrc
