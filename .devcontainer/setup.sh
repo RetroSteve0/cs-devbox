f50db284920adb64a2dae30c35ff3d9206ef11b8
Steven Mosley <34114851+RetroSteve0@users.noreply.github.com>
2023-08-10 14:18:27 +0000

    Removed nvm and pyenv from automated script as they are meant to be used from user level, not globally

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -15,18 +15,5 @@ execute_command() {
 execute_command sudo apt-get update -y
 execute_command sudo apt-get upgrade -y
 
-# Install NVM
-execute_command sudo git clone https://github.com/nvm-sh/nvm.git /usr/local/nvm
-execute_command sudo chown -R root:root /usr/local/nvm
-execute_command echo 'export NVM_DIR="/usr/local/nvm"' | sudo tee -a /etc/bash.bashrc
-execute_command echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' | sudo tee -a /etc/bash.bashrc
-execute_command echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"' | sudo tee -a /etc/bash.bashrc
-
-# Reload shell configuration (for the current session)
-execute_command sudo /bin/bash -c "source /etc/bash.bashrc"
-
-# Output a message indicating completion
-echo "Script execution completed."
-
 # Display the log file
 echo "Log file: $LOG_FILE"
